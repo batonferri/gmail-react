298ffda5cd04562d383b4538d13d910938ca2314
batonferri <72993088+batonferri@users.noreply.github.com>
2021-12-14 20:00:01 +0100

    Saving emails on db firebase

--- src/SendMail.js
+++ src/SendMail.js
@@ -1,25 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './SendMail.css'
 import CloseIcon from '@mui/icons-material/Close';
 import { Button } from '@mui/material';
-import {useForm} from "react-hook-form";
+// import {useForm} from "react-hook-form";
 import { useDispatch } from 'react-redux';
 import { closeSendMessage } from './features/mailSlice';
 import { db } from './firebase';
-import {addDoc, collection, doc, getDocs, setDoc} from "firebase/firestore"
+import {addDoc, collection, serverTimestamp} from "firebase/firestore"
 
 function SendMail() {
-
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const [newTo, setNewTo] = useState("");
+    const [newSubject, setNewSubject] = useState("");
+    const [newMessage, setNewMessage] = useState("");
+    // const { register, handleSubmit, watch, formState: { errors } } = useForm();
     const dispatch = useDispatch();
     const emailsCollection = collection(db, "emails");
 
-    const onSubmit = (formData) =>{
-        console.log(formData);
-        dispatch(closeSendMessage());    
-    };
+    // const onSubmit = (formData) =>{
+    //     console.log(formData);
+    //     dispatch(closeSendMessage());    
+    // };
 
-    
+    const sendEmail = async () => {
+        await addDoc(emailsCollection, 
+            {   name: newTo, 
+                subject : newSubject,
+                message:newMessage,
+                timestamp: serverTimestamp(),
+            })
+        dispatch(closeSendMessage());
+    };
 
     return (
         <div className='sendMail'>
@@ -28,21 +38,18 @@ function SendMail() {
                 <CloseIcon className='sendMail__close'
                 onClick={() => dispatch(closeSendMessage())}/>
             </div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <input name='to' placeholder='To' type="email" {...register("to", {required: true})}/>
-                {errors.to && <p className='sendMail__error'>To is Required!</p>}
+            <form>
+                <input name='to' placeholder='To' type="email" onChange={(event) => {setNewTo(event.target.value);}} />
 
-                <input name='subject' placeholder='Subject' type="text" {...register("subject", {required: true})}/>
-                {errors.to && <p className='sendMail__error'>Subject is Required!</p>}
+                <input name='subject' placeholder='Subject' type="text" onChange={(event) => {setNewSubject(event.target.value);}}/>
 
-                <input name='message' placeholder='Message...' type="text" className='sendMail__message' {...register("message", {required: true})} />
-                {errors.to && <p className='sendMail__error'>Message is Required!</p>}
+                <input name='message' placeholder='Message...' type="text" className='sendMail__message' onChange={(event) => {setNewMessage(event.target.value);}}/>
 
                 <div className="sendMail__options">
                     <Button className='sendMail__send'
                     variant='contained'
                     color='primary'
-                    type='submit'>Send</Button>
+                    onClick={sendEmail} >Send</Button>
                 </div>
             </form>
         </div>
